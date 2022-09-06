7b9159c6eb06717462fe28b783e2e853d7c3596d
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-09-06 15:20:55 +0200

    first commit

---
+++ db/setup.ts
@@ -0,0 +1,36 @@
+import Database from "better-sqlite3";
+const db = Database("./db/data.db", {verbose: console.log})
+
+const teacher = [
+    {
+        name: "Nicolas",
+        age:34
+    },
+    {
+        name: "Ed",
+        age:28
+    }
+]
+const deleteallteachers = db.prepare(`DELETE FROM teachers;
+`)
+deleteallteachers.run()
+
+const deleteallstudents = db.prepare(`DELETE FROM students;
+`)
+deleteallstudents.run()
+
+
+const createTeachersTable = db.prepare(`
+INSERT INTO teachers (name, age) VALUES (?,?);
+`
+)
+createTeachersTable.run('Nicolas', 34)
+createTeachersTable.run('Ed', 28)
+
+
+const createStudentsTable = db.prepare(`
+INSERT INTO students (name, age) VALUES (?,?);
+`
+)
+createStudentsTable.run('Edona',22)
+createStudentsTable.run('Arbenit',21)
